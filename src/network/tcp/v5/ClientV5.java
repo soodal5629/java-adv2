b35920098e046108e042bd0167eee27b1fef649b
jcl <chaelynjang@lotte.net>
2025-03-05 10:26:09 +0900

    try-with-resoutce 이용해서 깔끔하게 자원 정리 적용

---
+++ src/network/tcp/v5/ClientV5.java
@@ -0,0 +1,40 @@
+package network.tcp.v5;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+import static util.MyLogger.log;
+
+public class ClientV5 {
+    private static final int PORT = 12345;
+
+    public static void main(String[] args) throws IOException {
+        log("클라이언트 시작");
+        try (Socket socket = new Socket("localhost", PORT);
+             DataInputStream input = new DataInputStream(socket.getInputStream());
+             DataOutputStream output = new DataOutputStream(socket.getOutputStream())) {
+            log("소켓 연결: " + socket);
+
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                System.out.print("전송 문자: ");
+                // 서버에 문자 보내기
+                String toSend = scanner.nextLine();
+                output.writeUTF(toSend);
+                log("client -> server: " + toSend);
+                if (toSend.equals("exit")) {
+                    break;
+                }
+                // 서버로부터 문자 받기
+                String received = input.readUTF();
+                log("server -> client: " + received);
+            }
+        } catch(IOException e) {
+            log(e);
+        }
+    }
+
+}
