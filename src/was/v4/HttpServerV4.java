c19e273eb20907d157d71d2fc5a8acbdb4210f61
jcl <chaelynjang@lotte.net>
2025-04-21 14:29:16 +0900

    HttpRequest, HttpResponse 객체 만들어서 코드 리팩토링

---
+++ src/was/v4/HttpServerV4.java
@@ -0,0 +1,30 @@
+package was.v4;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static util.MyLogger.log;
+
+public class HttpServerV4 {
+    // 스레드 풀 사용 - 최대 스레드 10개 사용
+    private final ExecutorService es = Executors.newFixedThreadPool(10);
+    private final int port;
+
+    public HttpServerV4(int port) {
+        this.port = port;
+    }
+
+    public void start() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(port);
+        log("서버 시작 port: " + port);
+
+        while (true) {
+            Socket socket = serverSocket.accept();
+            es.submit(new HttpRequestHandlerV4(socket));
+        }
+    }
+
+}
